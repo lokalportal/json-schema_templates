01eea9bb116713c982b1713feaccdb418af0a330
Stefan Exner <stex@sterex.de>
2019-03-01 14:45:52 +0100

    Added lookup method for a specific schema

--- lib/json/schema_templates.rb
+++ lib/json/schema_templates.rb
@@ -19,5 +19,19 @@ module JSON
     def self.configure
       yield(configuration)
     end
+
+    #
+    # @return [Hash, nil] The requested schema as JSON hash or +nil+ if no such schema could be found
+    #
+    # @param [String] schema_path
+    #   The requested schema relative to the schema base path.
+    #
+    # @example
+    #   Given the schema base path 'schemas':
+    #   JSON::SchemaTemplates.json_schema_for('posts/show') #=> ::Schemas::Posts::Show.new.schema.as_json
+    #
+    def self.json_schema_for(schema_path)
+      "/#{configuration.base_path}/#{schema_path}".camelize.safe_constantize&.new&.schema&.as_json
+    end
   end
 end
