6029da81c87264b038960faa53b18082e0145e3c
Paul Martensen <paul.martensen@gmx.de>
2019-10-15 10:52:26 +0200

    Integrated template config with dsl config.

--- lib/json/schema_templates/configuration.rb
+++ lib/json/schema_templates/configuration.rb
@@ -3,19 +3,31 @@
 module JSON
   module SchemaTemplates
     class Configuration
+      delegate :add_defaults_for, :type_defaults, to: ::JSON::SchemaDsl
+
       # Sets a default value for `additional_properties` on objects used in schemas
       # Setting it to `nil` results in the key not being present in the resulting schema
       def additional_properties_on_objects=(bool)
-        ::JSON::SchemaDsl.add_defaults_for(:object, additional_properties: bool)
+        add_defaults_for(:object, additional_properties: bool || false)
       end
 
       def additional_properties_on_objects
-        ::JSON::SchemaDsl.type_defaults.dig(:object, :additional_properties)
+        type_defaults.dig(:object, :additional_properties)
       end
 
       # Every generated schema is wrapped in a root object.
       # This setting controls whether this root object is allowed to have additional properties
-      attr_accessor :additional_properties_on_base_object
+      def additional_properties_on_base_object=(bool)
+        add_defaults_for(:base_object, additional_properties: bool || false)
+      end
+
+      def additional_properties_on_base_object
+        type_defaults.dig(:base_object, :additional_properties)
+      end
+
+      def defaults_for(subject)
+        type_defaults[subject]
+      end
 
       # The schema base path.
       # As every schema is a class and part of a module hierarchy, setting it to
@@ -36,12 +48,6 @@ module JSON
         reset!
       end
 
-      def defaults_for(subject)
-        return defaults[subject.to_sym] if subject.to_sym == :base_object
-
-        ::JSON::SchemaDsl.type_defaults[subject]
-      end
-
       #
       # @return [Module] the root module the application's schemas reside in (based on #base_path)
       #
@@ -56,19 +62,10 @@ module JSON
       # Resets the configuration back to its default values
       #
       def reset!
-        self.additional_properties_on_objects = nil
-        self.additional_properties_on_base_object = nil
+        type_defaults.clear
         self.base_path = 'schemas'
         self.cache_schemas = false
         self.schema_id_prefix = 'https://example.com'
-      end
-
-      def defaults
-        {
-          base_object: {
-            additional_properties: additional_properties_on_base_object
-          }
-        }.transform_values(&:compact)
       end
     end
   end
